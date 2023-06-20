8adb96d1e0161c6b7d5a2579ff98b82e459acaa2
John Corn <jcornacchioli@gmail.com>
2023-06-20 13:45:30 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-function isPalindrome(word) {
+function isPalindrome(word) 
+{
   // Write your algorithm here
+  console.log(FlipWord(word));
+  return (word == FlipWord(word));
 }
 
+function FlipWord(word)
+{
+  let splitString = word.split(""); 
+  let reverseArray = splitString.reverse(); 
+  let joinArray = reverseArray.join(""); 
+  
+  return joinArray; 
+}
 /* 
   Add your pseudocode here
+  make a var of the same word flipped
+  directly compare the strings
 */
 
 /*
   Add written explanation of your solution here
+  we just making the word backwords and seeing if its the same as the word forward
 */
 
 // You can run `node index.js` to view these console logs
